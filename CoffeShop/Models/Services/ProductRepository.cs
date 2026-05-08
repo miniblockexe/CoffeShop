a0f6feee1ad0d7cf6201f1f58f00e17162fcacf6
Trần Tâm <trantamttkmini@gmail.com>
2026-05-08 09:40:26 +0700

    Add project files.

---
+++ CoffeShop/Models/Services/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using CoffeShop.Data;
+using CoffeShop.Models.Interfaces;
+
+namespace CoffeShop.Models.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> ProductsList = new List<Product>()
+    {
+        new Product { Id = 1, Name = "America", Price = 25, Detail = "Name product", ImageUrl = "https://index.com" },
+        new Product { Id = 2, Name = "Vietnam", Price = 20, Detail = "Vietnamese product", ImageUrl = "https://index.com" },
+        new Product { Id = 3, Name = "United Kingdom", Price = 15, Detail = "Name product", ImageUrl = "https://index.com" }
+    };
+
+        //public IEnumerable<Product> GetAllProducts()
+        //{
+        //    return ProductsList;
+        //}
+
+        //public Product GetProductDetail(int id)
+        //{
+        //    return ProductsList.FirstOrDefault(p => p.Id == id);
+        //}
+
+        //public IEnumerable<Product> GetTrendingProducts()
+        //{
+        //    return ProductsList.Where(p => p.IsTrendingProduct);
+        //}
+
+        private CoffeeshopDbContext dbContext;
+
+        public ProductRepository(CoffeeshopDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return dbContext.Products;
+        }
+
+        public Product? GetProductDetail(int id)
+        {
+            return dbContext.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Product> GetTrendingProducts()
+        {
+            return dbContext.Products.Where(p => p.IsTrendingProduct);
+        }
+    }
+}
---
+++ CoffeShop/Program.cs
@@ -0,0 +1,33 @@
+using CoffeShop.Data;
+using CoffeShop.Models.Interfaces;
+using CoffeShop.Models.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CoffeeshopDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("CoffeeShopDbContextConnection")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
